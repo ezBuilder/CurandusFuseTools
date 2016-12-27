95959c12f268ed883d1591979ccdfe3da018984b
Bojan Janevski <bojan.janevski@interway.com.mk>
2016-12-27 13:49:33 +0100

    quickfix

--- Pages/MainPage/MainPage.js
+++ Pages/MainPage/MainPage.js
@@ -1,7 +1,5 @@
 var Observable = require("FuseJS/Observable");
 
-var contactsView = require("ContactsView/ContactsView");
-
 var contacts = Observable();
 var errorMessage = Observable();
 var isLoading = Observable(false);
@@ -14,11 +12,11 @@ function toolbarSearch() {
     console.log('da');
 }
 
+
 this.Parameter.onValueChanged(module, function(param) {
     // lookup channel with id "param"
     console.log("We should RELOAD " + JSON.stringify(param.reload));
 
-    contactsView.fetchDataDoctors();
 
 });
 
