3f6f8e80b3cea53d97b08b4a3069f8db2eb462ff
BojanJ <bojan.janevski@interway.com.mk>
2017-01-31 13:55:58 +0100

    qfix

--- Pages/AlertPage/AlertPage.js
+++ Pages/AlertPage/AlertPage.js
@@ -8,187 +8,158 @@
 		var nameLastname = Observable();
 		var chat = Observable();
 		var treatmentItems = Observable();
-		var templejt=Observable();
-		var firstID=0;
-		var lastID=0;
+		var templejt = Observable();
+		var firstID = 0;
+		var lastID = 0;
 
 
 
-		function loadMore()
-		{
+		function loadMore() {
 
-		console.log("LOAD");
-		fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlistscroll/treatmentitemlistid="+ lastID +"&updown=D&range=10", {
-		    method: 'GET',
-		    headers: {
-		        "Content-type": "application/json"
-		    },
-		    dataType: 'json'
-		}).then(function(response) {
-		    status = response.status; // Get the HTTP status code
-		    response_ok = response.ok; // Is response.status in the 200-range?
-		    return response.json(); // This returns a promise
-		}).then(function(responseObject) {
-		    console.log("Success");
-		    console.log(JSON.stringify(responseObject));
+		    console.log("LOAD");
+		    fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlistscroll/treatmentitemlistid=" + lastID + "&updown=D&range=10", {
+		        method: 'GET',
+		        headers: {
+		            "Content-type": "application/json"
+		        },
+		        dataType: 'json'
+		    }).then(function(response) {
+		        status = response.status; // Get the HTTP status code
+		        response_ok = response.ok; // Is response.status in the 200-range?
+		        return response.json(); // This returns a promise
+		    }).then(function(responseObject) {
+		        console.log("Success");
+		        console.log(JSON.stringify(responseObject));
 
-		    lastID=responseObject[responseObject.length-1].treatmentItemListId;
+		        lastID = responseObject[responseObject.length - 1].treatmentItemListId;
 
-		    for (var i = 0; i < responseObject.length; i++) {
-		        
+		        for (var i = 0; i < responseObject.length; i++) {
 
-		        var tmpDate = new Date(responseObject[i].timeScheduled);
 
-		    	var days = tmpDate.getDate();
-		    	var months = tmpDate.getMonth()+1;
-		    	var year = tmpDate.getFullYear();
-		    	var hours = tmpDate.getHours();
-		    	var min = tmpDate.getMinutes();
+		            var tmpDate = new Date(responseObject[i].timeScheduled);
 
-		    	var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth()+1)).slice(-2) + '.' + tmpDate.getFullYear();
-		    	var fulltime = ('0'  + hours).slice(-2)+':'+('0' + min).slice(-2);
+		            var days = tmpDate.getDate();
+		            var months = tmpDate.getMonth() + 1;
+		            var year = tmpDate.getFullYear();
+		            var hours = tmpDate.getHours();
+		            var min = tmpDate.getMinutes();
 
-		    	responseObject[i].fullDate = fullDate;
-		    	responseObject[i].fullTime = fulltime;
+		            var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth() + 1)).slice(-2) + '.' + tmpDate.getFullYear();
+		            var fulltime = ('0' + hours).slice(-2) + ':' + ('0' + min).slice(-2);
 
-		        templejt.add(responseObject[i])
-		    }
+		            responseObject[i].fullDate = fullDate;
+		            responseObject[i].fullTime = fulltime;
 
-		}).catch(function(err) {
-		    console.log("Error", err.message);
+		            templejt.add(responseObject[i])
+		        }
 
-		});
+		    }).catch(function(err) {
+		        console.log("Error", err.message);
+
+		    });
 		}
 
 
-		function loadMore1()
-		{
+		function loadMore1() {
 
-		console.log("LOAD111111");
-		fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlistscroll/treatmentitemlistid="+ firstID +"&updown=U&range=10", {
-		    method: 'GET',
-		    headers: {
-		        "Content-type": "application/json"
-		    },
-		    dataType: 'json'
-		}).then(function(response) {
-		    status = response.status; // Get the HTTP status code
-		    response_ok = response.ok; // Is response.status in the 200-range?
-		    return response.json(); // This returns a promise
-		}).then(function(responseObject) {
-		    console.log("Success");
-		    console.log(JSON.stringify(responseObject));;
+		    console.log("LOAD111111");
+		    fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlistscroll/treatmentitemlistid=" + firstID + "&updown=U&range=10", {
+		        method: 'GET',
+		        headers: {
+		            "Content-type": "application/json"
+		        },
+		        dataType: 'json'
+		    }).then(function(response) {
+		        status = response.status; // Get the HTTP status code
+		        response_ok = response.ok; // Is response.status in the 200-range?
+		        return response.json(); // This returns a promise
+		    }).then(function(responseObject) {
+		        console.log("Success");
+		        console.log(JSON.stringify(responseObject));;
 
-		    	firstID = responseObject[0].treatmentItemListId;
+		        firstID = responseObject[0].treatmentItemListId;
 
-		    	for (var i = responseObject.length - 1; i >= 0; i--) {
+		        for (var i = responseObject.length - 1; i >= 0; i--) {
 
-		        var tmpDate = new Date(responseObject[i].timeScheduled);
+		            var tmpDate = new Date(responseObject[i].timeScheduled);
 
-		    	var days = tmpDate.getDate();
-		    	var months = tmpDate.getMonth()+1;
-		    	var year = tmpDate.getFullYear();
-		    	var hours = tmpDate.getHours();
-		    	var min = tmpDate.getMinutes();
+		            var days = tmpDate.getDate();
+		            var months = tmpDate.getMonth() + 1;
+		            var year = tmpDate.getFullYear();
+		            var hours = tmpDate.getHours();
+		            var min = tmpDate.getMinutes();
 
-		    	var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth()+1)).slice(-2) + '.' + tmpDate.getFullYear();
-		    	var fulltime = ('0'  + hours).slice(-2)+':'+('0' + min).slice(-2);
+		            var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth() + 1)).slice(-2) + '.' + tmpDate.getFullYear();
+		            var fulltime = ('0' + hours).slice(-2) + ':' + ('0' + min).slice(-2);
 
-		    	responseObject[i].fullDate = fullDate;
-		    	responseObject[i].fullTime = fulltime;
-		    	
-		        templejt.insertAt(0, responseObject[i])
-		    }
+		            responseObject[i].fullDate = fullDate;
+		            responseObject[i].fullTime = fulltime;
+
+		            templejt.insertAt(0, responseObject[i])
+		        }
 
 
-		}).catch(function(err) {
-		    console.log("Error", err.message);
+		    }).catch(function(err) {
+		        console.log("Error", err.message);
 
-		});
+		    });
 		}
 
-		function initload()
-		{
+		function initload() {
 
-		console.log("LOAD");
-		fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlis/activetreatmentid=15", {
-		    method: 'GET',
-		    headers: {
-		        "Content-type": "application/json"
-		    },
-		    dataType: 'json'
-		}).then(function(response) {
-		    status = response.status; // Get the HTTP status code
-		    response_ok = response.ok; // Is response.status in the 200-range?
-		    return response.json(); // This returns a promise
-		}).then(function(responseObject) {
-		    console.log("Success");
-		    console.log(JSON.stringify(responseObject));
-		    firstID=responseObject[0].treatmentItemListId;
-		    lastID=responseObject[responseObject.length-1].treatmentItemListId;
-		    console.log(firstID,lastID);
-		    for (var i = 0; i < responseObject.length; i++) {
-		        
-		    	var tmpDate = new Date(responseObject[i].timeScheduled);
+		    console.log("LOAD");
+		    fetch("http://192.168.1.110:8080/curandusproject/webapi/api/treatmentitemlis/activetreatmentid=15", {
+		        method: 'GET',
+		        headers: {
+		            "Content-type": "application/json"
+		        },
+		        dataType: 'json'
+		    }).then(function(response) {
+		        status = response.status; // Get the HTTP status code
+		        response_ok = response.ok; // Is response.status in the 200-range?
+		        return response.json(); // This returns a promise
+		    }).then(function(responseObject) {
+		        console.log("Success");
+		        console.log(JSON.stringify(responseObject));
+		        firstID = responseObject[0].treatmentItemListId;
+		        lastID = responseObject[responseObject.length - 1].treatmentItemListId;
+		        console.log(firstID, lastID);
+		        for (var i = 0; i < responseObject.length; i++) {
 
-		    	var days = tmpDate.getDate();
-		    	var months = tmpDate.getMonth()+1;
-		    	var year = tmpDate.getFullYear();
-		    	var hours = tmpDate.getHours();
-		    	var min = tmpDate.getMinutes();
+		            var tmpDate = new Date(responseObject[i].timeScheduled);
 
-		    	var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth()+1)).slice(-2) + '.' + tmpDate.getFullYear();
-		    	var fulltime = ('0'  + hours).slice(-2)+':'+('0' + min).slice(-2);
+		            var days = tmpDate.getDate();
+		            var months = tmpDate.getMonth() + 1;
+		            var year = tmpDate.getFullYear();
+		            var hours = tmpDate.getHours();
+		            var min = tmpDate.getMinutes();
 
-		    	responseObject[i].fullDate = fullDate;
-		    	responseObject[i].fullTime = fulltime;
+		            var fullDate = ('0' + tmpDate.getDate()).slice(-2) + '.' + ('0' + (tmpDate.getMonth() + 1)).slice(-2) + '.' + tmpDate.getFullYear();
+		            var fulltime = ('0' + hours).slice(-2) + ':' + ('0' + min).slice(-2);
 
-		    	console.log(fullDate, fulltime);
+		            responseObject[i].fullDate = fullDate;
+		            responseObject[i].fullTime = fulltime;
 
-		        templejt.add(responseObject[i])
-		    }
+		            console.log(fullDate, fulltime);
 
-		   
+		            templejt.add(responseObject[i])
+		        }
 
-		}).catch(function(err) {
-		    console.log("Error", err.message);
 
-		});
 
-		
-				 
+		    }).catch(function(err) {
+		        console.log("Error", err.message);
+
+		    });
+
+
+
 		}
 		initload();
 
-		// fetch("http://192.168.1.165:8081/curandusproject/webapi/api/gettreatmentitemlist/3", {
-		//     method: 'GET',
-		//     headers: {
-		//         "Content-type": "application/json"
-		//     },
-		//     dataType: 'json'
-		// }).then(function(response) {
-		//     status = response.status; // Get the HTTP status code
-		//     response_ok = response.ok; // Is response.status in the 200-range?
-		//     return response.json(); // This returns a promise
-		// }).then(function(responseObject) {
-		//     console.log("Success");
-		//     console.log(JSON.stringify(responseObject));;
-		//     for (var i = 0; i < responseObject.length; i++) {
-		//         responseObject[i].render = JSON.parse(responseObject[i].renderingInfo);
-		//         console.log("render", JSON.stringify(responseObject[i].render));
-		//         treatmentItems.add(responseObject[i])
-		//     }
-
-		// }).catch(function(err) {
-		//     console.log("Error", err.message);
-
-		// });
-
 		function statusFunc(e) {
 
-
-			console.log(JSON.stringify(e.data.treatmentItemListId));
-
+		    console.log(JSON.stringify(e.data.treatmentItemListId));
 
 		    treatmentItemID = JSON.stringify(e.data.treatmentItemListId);
 
@@ -204,24 +175,24 @@
 		            if (s == "Yes") {
 
 
-					    fetch("http://192.168.1.165:8081/curandusproject/webapi/api/updatetreatmentitemlist/" + treatmentItemID, {
-					        method: 'PUT',
-					        headers: {
-					            "Content-type": "application/json"
-					        },
-					        dataType: 'json',
-					        body: JSON.stringify(status)
-					    }).then(function(response) {
-					        status = response.status; // Get the HTTP status code
-					        response_ok = response.ok; // Is response.status in the 200-range?
-					        return response.json(); // This returns a promise
-					    }).then(function(responseObject) {
-					        console.log("Success");
+		                fetch("http://192.168.1.165:8081/curandusproject/webapi/api/updatetreatmentitemlist/" + treatmentItemID, {
+		                    method: 'PUT',
+		                    headers: {
+		                        "Content-type": "application/json"
+		                    },
+		                    dataType: 'json',
+		                    body: JSON.stringify(status)
+		                }).then(function(response) {
+		                    status = response.status; // Get the HTTP status code
+		                    response_ok = response.ok; // Is response.status in the 200-range?
+		                    return response.json(); // This returns a promise
+		                }).then(function(responseObject) {
+		                    console.log("Success");
 
-					    }).catch(function(err) {
-					        console.log("Error", err.message);
+		                }).catch(function(err) {
+		                    console.log("Error", err.message);
 
-					    });
+		                });
 
 		            }
 		        });
